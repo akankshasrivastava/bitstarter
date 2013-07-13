37f7ce71f50272bc2e46d967e66fc49402538fbb
Akanksha <akankshasrivastava@gmail.com>
2013-07-13 02:53:27 +0000

    Added input from index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs.readFile('/bitstarter/index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+y = buf.toString()
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(y);
 });
 
 var port = process.env.PORT || 5000;

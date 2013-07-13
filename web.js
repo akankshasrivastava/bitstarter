d1aa76bc7e8e7fa482388c319bae7d591bf6470e
Akanksha <akankshasrivastava@gmail.com>
2013-07-13 03:21:26 +0000

    Added input from index.html correctly hopefully finally

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var buffer = new Buffer(150);
 
-fs.readFile('/bitstarter/index.html', buffer, function (err) {
+fs.writeFileSync('/bitstarter/index.html', buffer, function (err) {
   if (err) throw err;
 console.log('Saved it');	
 });

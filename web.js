62963bc2ff129820ddf5243ad193fbd36de35b83
Akanksha Srivastava <akankshasrivastava@gmail.com>
2013-07-23 05:14:43 +0000

    populated with travelog content

--- web.js
+++ web.js
@@ -10,6 +10,12 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
+var img = fs.readFileSync('./travelogo.jpg');
+app.get('/travelogo.jpg', function(request, response) {
+  response.writeHead(200, {'Content-Type': 'image/jpeg' });
+  response.end(img, 'binary');
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
